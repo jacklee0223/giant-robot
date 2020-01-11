ff4913edbf786f6b9aff5f4b4f1a2cf98c4fab4a
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-11 13:14:37 -0600

    handle typing after error

--- src/components/InfoForm.jsx
+++ src/components/InfoForm.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import './InfoForm.css';
 
@@ -14,6 +15,27 @@ export default class InfoForm extends Component {
     };
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    const { missingRequiredFields } = this.state;
+    const requiredFields = ['firstName', 'lastName', 'address'];
+
+    if (_.isEqual(prevState, this.state)) {
+      return;
+    }
+
+    if (missingRequiredFields.length) {
+      const missingFields = missingRequiredFields.slice();
+
+      _.map(requiredFields, field => {
+        if (this.state[field].length) {
+          _.pull(missingFields, field);
+        }
+      });
+
+      this.setState({ missingRequiredFields: missingFields });
+    }
+  }
+
   onInputChange = (e, fieldName) => {
     e.preventDefault();
     const { value } = e.target;
@@ -24,32 +46,67 @@ export default class InfoForm extends Component {
   };
 
   handleNext = () => {
-    const { firstName, lastName, address, addressTwo } = this.state;
+    const { firstName, lastName, address, missingRequiredFields } = this.state;
+    const hasAllRequired =
+      firstName.length && lastName.length && address.length;
+    const missingFields = [];
+
+    if (!hasAllRequired) {
+      const requiredFields = ['firstName', 'lastName', 'address'];
+
+      _.map(requiredFields, field => {
+        if (this.state[field].length < 1) {
+          missingFields.push(field);
+        }
+      });
+
+      this.setState({ missingRequiredFields: missingFields });
+    }
+
+    if (!missingRequiredFields.length && hasAllRequired) {
+      this.setState({ missingRequiredFields: [] });
+    }
+  };
+
+  renderInput = inputName => {
+    const { missingRequiredFields } = this.state;
+
+    return (
+      <input
+        className={`${this.state[inputName].length > 0 ? 'has-input' : ''} ${
+          _.includes(missingRequiredFields, inputName) ? 'required-missing' : ''
+        }`}
+        onChange={e => this.onInputChange(e, inputName)}
+      />
+    );
   };
 
   render() {
-    const { firstName, lastName, address, addressTwo } = this.state;
+    const {
+      firstName,
+      lastName,
+      address,
+      addressTwo,
+      missingRequiredFields
+    } = this.state;
 
     return (
       <div className="info-form">
         <div className="label-input-container">
           <label>FIRST NAME</label>
-          <input
-            className={firstName.length > 0 ? 'has-input' : ''}
-            onChange={e => this.onInputChange(e, 'firstName')}
-          />
+          {this.renderInput('firstName')}
         </div>
         <div className="label-input-container">
           <label>LAST NAME</label>
-          <input onChange={e => this.onInputChange(e, 'lastName')} />
+          {this.renderInput('lastName')}
         </div>
         <div className="label-input-container">
           <label>ADDRESS</label>
-          <input onChange={e => this.onInputChange(e, 'address')} />
+          {this.renderInput('address')}
         </div>
         <div className="label-input-container">
           <label>ADDRESS 2 (OPTIONAL)</label>
-          <input onChange={e => this.onInputChange(e, 'addressTwo')} />
+          {this.renderInput('addressTwo')}
         </div>
         <button className="next-button" onClick={this.handleNext}>
           NEXT →
