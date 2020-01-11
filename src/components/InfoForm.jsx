ff964587e52460e9b58f385e148f8a286f4bc99e
Jeong Min Lee <jacklee0223@gmail.com>
2020-01-11 01:04:37 -0600

    implement state and oninputchange

--- src/components/InfoForm.jsx
+++ src/components/InfoForm.jsx
@@ -4,14 +4,30 @@ import './InfoForm.css';
 export default class InfoForm extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      address: '',
+      addressTwo: ''
+    };
   }
+
+  onInputChange = (e, fieldName) => {
+    e.preventDefault();
+    const { value } = e.target;
+
+    this.setState({
+      [fieldName]: value
+    });
+  };
 
   render() {
     return (
       <div className="info-form">
         <div className="label-input-container">
           <label>Name</label>
-          <input />
+          <input onChange={e => this.onInputChange(e, 'firstName')} />
         </div>
       </div>
     );
